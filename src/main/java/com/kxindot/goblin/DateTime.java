2e11c4d844f77c090a19317363237966181df562
赵清江 <livingdarkmind@163.com>
2022-08-03 14:56:26 +0800

    新增日期/时间工具类(待完成)

---
+++ src/main/java/com/kxindot/goblin/DateTime.java
@@ -0,0 +1,193 @@
+package com.kxindot.goblin;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * 日期及时间工具类
+ * @author ZhaoQingJiang
+ */
+public final class DateTime extends Date {
+    
+    private static final long serialVersionUID = -8695262811586544228L;
+
+    /** 时区 */
+    private TimeZone timeZone;
+
+    
+    public DateTime() {
+        super();
+    }
+    
+    
+    public DateTime(long timestamp) {
+        super(timestamp);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static Date toDate(String text) {
+        
+        return null;
+    }
+    
+    
+    public static Date toDate(String text, String pattern) {
+        
+        return null;
+    }
+    
+    
+    public static LocalDateTime toLocalDateTime(Date date) {
+        
+        return null;
+    }
+    
+    
+    public static LocalDateTime toLocalDateTime(Date date, String pattern) {
+        
+        return null;
+    }
+    
+    
+    public static LocalDateTime toLocalDateTime(String text) {
+        
+        return null;
+    }
+    
+    
+    public static LocalDateTime toLocalDateTime(String text, String pattern) {
+        
+        return null;
+    }
+    
+    
+    
+    public static LocalDate toLocalDate(Date date) {
+        
+        return null;
+    }
+    
+    
+    public static LocalDate toLocalDate(Date date, String pattern) {
+        
+        return null;
+    }
+    
+    
+    public static LocalDate toLocalDate(String text) {
+        
+        return null;
+    }
+    
+    
+    public static LocalDate toLocalDate(String text, String pattern) {
+        
+        return null;
+    }
+    
+    
+    public static LocalTime toLocalTime(Date date) {
+        
+        return null;
+    }
+    
+    
+    public static LocalTime toLocalTime(Date date, String pattern) {
+        
+        return null;
+    }
+    
+    
+    public static LocalTime toLocalTime(String text) {
+        
+        return null;
+    }
+    
+    
+    public static LocalTime toLocalTime(String text, String pattern) {
+        
+        return null;
+    }
+    
+    
+    public static String toString(Date date) {
+        
+        return null;
+    }
+    
+    
+    public static String toString(Date date, String pattern) {
+        
+        return null;
+    }
+    
+    
+    public static String toString(LocalDateTime date) {
+        
+        
+        return null;
+    }
+    
+    
+    public static String toString(LocalDateTime date, String pattern) {
+        
+        return null;
+    }
+    
+    
+    public static String toString(LocalDate date) {
+        
+        
+        return null;
+    }
+    
+    
+    public static String toString(LocalDate date, String pattern) {
+        
+        
+        return null;
+    }
+    
+    
+    public static String toString(LocalTime time) {
+        
+        
+        return null;
+    }
+    
+    
+    public static String toString(LocalTime time, String pattern) {
+        
+        
+        return null;
+    }
+    
+    
+}
