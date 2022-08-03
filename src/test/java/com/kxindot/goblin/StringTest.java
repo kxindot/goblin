332e8ea59b3cf73215c3fc81a138a00657b79829
赵清江 <livingdarkmind@163.com>
2022-08-03 14:54:25 +0800

    新增字符串处理测试类

---
+++ src/test/java/com/kxindot/goblin/StringTest.java
@@ -0,0 +1,137 @@
+package com.kxindot.goblin;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+
+class StringTest {
+    
+    Logger logger;
+
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+    }
+
+    @Test
+    void testIsAnyBlank() {
+        logger.debug("logger test");
+    }
+
+    @Test
+    void testIsAllBlank() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testIsNotBlank() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testIsAnyNotBlank() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testIsAllNotBlank() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testIsNumeric() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testDecapitalize() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testStringFormat() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testStringJoinCharSequenceArray() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testStringJoinObjectArray() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testStringJoinIterableOfQ() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testStringJoinWithCharSequenceCharSequenceArray() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testStringJoinWithCharSequenceObjectArray() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testStringJoinWithCharSequenceIterableOfQ() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSubstringCharSequenceInt() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSubstringCharSequenceIntInt() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSubstringLeft() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSubstringRight() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSubstringBefore() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSubstringAfter() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSubstringBeforeLast() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSubstringAfterLast() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSubstringBetween() {
+        fail("Not yet implemented");
+    }
+
+}
