f8117b8a4570d91f7d5dbb63e83e945567f15457
赵清江 <livingdarkmind@163.com>
2022-08-03 14:57:00 +0800

    新增若干待完善方法

--- src/main/java/com/kxindot/goblin/Beans.java
+++ src/main/java/com/kxindot/goblin/Beans.java
@@ -1,9 +1,36 @@
 package com.kxindot.goblin;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author ZhaoQingJiang
  */
 public class Beans {
+    
+    public static void copy(Object source, Object target) {
+        
+    }
 
+    public static <T> T copyOf(Object source, Class<T> type) {
+        
+        return null;
+    }
+    
+    public static <E> List<E> copyOf(List<?> source, Class<E> type) {
+        
+        return null;
+    }
+    
+    public static <E> Set<E> copyOf(Set<?> source, Class<E> type) {
+        
+        return null;
+    }
+    
+    public static <E> Set<E> copyOf(Collection<?> source, Class<E> type) {
+        
+        return null;
+    }
 	
 }
