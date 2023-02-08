0ed5f1ef30144241cbe3183559ada040bac256b1
ZYY <livingdarkmind@163.com>
2023-02-08 17:40:27 +0800

    测试: 添加公共测试工具

---
+++ src/test/java/com/kxindot/goblin/testkit/JunitTests.java
@@ -0,0 +1,38 @@
+package com.kxindot.goblin.testkit;
+
+import static com.kxindot.goblin.Objects.stringRepeat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+/**
+ * @author ZhaoQingJiang
+ */
+@TestInstance(Lifecycle.PER_CLASS)
+public abstract class JunitTests extends LogTests {
+    
+    private String ba = stringRepeat(">", 30);
+    private String aa = stringRepeat("<", 30);
+//    private String abe = stringRepeat("-", 30);
+
+    @BeforeAll
+    public void beforeAll() {
+        logger.info(ba + getClass().getSimpleName() + ba);
+    }
+    
+    @AfterAll
+    public void afterAll() {
+        logger.info(aa + getClass().getSimpleName() + aa);
+    }
+    
+    @BeforeEach
+    public abstract void beforeEach();
+    
+    @AfterEach
+    public abstract void afterEach();
+    
+}
