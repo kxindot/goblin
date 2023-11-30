744422252b737d0228531b0706efdc73367ac761
snagon <snagon@yeah.net>
2023-11-30 14:54:03 +0800

    新增: 已检IOException

---
+++ src/main/java/com/kxindot/goblin/io/IIOException.java
@@ -0,0 +1,26 @@
+package com.kxindot.goblin.io;
+
+/**
+ * @author ZhaoQingJiang
+ */
+public class IIOException extends RuntimeException {
+
+	private static final long serialVersionUID = 4198265608056316377L;
+
+	public IIOException() {
+		super();
+	}
+
+	public IIOException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public IIOException(String message) {
+		super(message);
+	}
+
+	public IIOException(Throwable cause) {
+		super(cause);
+	}
+	
+}
