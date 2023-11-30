2a2a01d25071c40b0bc5d96ce7fcf09a8d4eeba2
snagon <snagon@yeah.net>
2023-11-30 14:54:52 +0800

    优化: 删除文件时不通知问题

--- src/main/java/com/kxindot/goblin/io/FileWatcher.java
+++ src/main/java/com/kxindot/goblin/io/FileWatcher.java
@@ -2,34 +2,46 @@ package com.kxindot.goblin.io;
 
 import static com.kxindot.goblin.Objects.isNotNull;
 import static com.kxindot.goblin.Objects.newArrayList;
+import static com.kxindot.goblin.Objects.newConcurrentHashMap;
 import static com.kxindot.goblin.Objects.requireNotNull;
 import static com.kxindot.goblin.Objects.requireTrue;
 import static com.kxindot.goblin.Resources.exists;
 import static com.kxindot.goblin.Resources.isDirectory;
 import static com.kxindot.goblin.Resources.isFile;
 import static com.kxindot.goblin.Throws.silentThrex;
+import static com.kxindot.goblin.Throws.threx;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchEvent.Kind;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import com.kxindot.goblin.logger.Logger;
+import com.kxindot.goblin.logger.LoggerFactory;
 
 /**
  * @author ZhaoQingJiang
  */
 public class FileWatcher {
 
+	private static Logger logger = LoggerFactory.getLogger(FileWatcher.class);
 	private Path directory;
 	private WatchService service;
 	private List<Whistle> whistles;
+	private Map<Path, Boolean> cache;
 	
 	public FileWatcher(Path directory) {
 		requireNotNull(directory, "directory == null");
@@ -42,7 +54,23 @@ public class FileWatcher {
 		} catch (IOException e) {
 			silentThrex(e);
 		}
+		this.cache = newConcurrentHashMap();
+		buildCache(directory);
 		this.whistles = Collections.synchronizedList(newArrayList());
+	}
+	
+	private void buildCache(Path path) {
+		this.cache.put(path, isFile(path));
+		if (isDirectory(path)) {
+			try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+	            Iterator<Path> iterator = stream.iterator();
+	            while (iterator.hasNext()) {
+	            	buildCache((Path) iterator.next());
+	            }
+	        } catch (IOException e) {
+	        	threx(IIOException::new, e);
+	        }
+		}
 	}
 	
 	public void add(Whistle whistle) {
@@ -61,7 +89,11 @@ public class FileWatcher {
 			}
 			if (key.isValid()) {
 				for (WatchEvent<?> event : key.pollEvents()) {
-					whistle(event);
+					try {
+						whistle(event);
+					} catch (Throwable e) {
+						logger.error("", e);
+					}
 				}
 			}
 			if (!key.reset()) {
@@ -84,26 +116,28 @@ public class FileWatcher {
 		for (Whistle whistle : whistles) {
 			Kind<?> kind = event.kind();
 			Path path = directory.resolve((Path) event.context());
-			if (isFile(path)) {
-				if (kind == ENTRY_CREATE) {
-					whistle.onFileCreated(path);
-				} else if (kind == ENTRY_DELETE) {
-					whistle.onFileDeteled(path);
-				} else if (kind == ENTRY_MODIFY) {
-					whistle.onFileModified(path);
-				}
-			} else if (isDirectory(path)) {
-				if (kind == ENTRY_CREATE) {
-					whistle.onDirectoryCreated(path);
-				} else if (kind == ENTRY_DELETE) {
-					whistle.onDirectoryDeleted(path);
-				} else if (kind == ENTRY_MODIFY) {
-					whistle.onDirectoryModified(path);
-				}
+			boolean isFile = isFile(path);
+			if (kind == ENTRY_CREATE) {
+				notify(isFile, path, whistle::onFileCreated, whistle::onDirectoryCreated);
+				this.cache.put(path, isFile(path));
+			} else if (kind == ENTRY_DELETE) {
+				isFile = this.cache.get(path);
+				notify(isFile, path, whistle::onFileDeteled, whistle::onDirectoryDeleted);
+				this.cache.remove(path);
+			} else if (kind == ENTRY_MODIFY) {
+				notify(isFile, path, whistle::onFileModified, whistle::onDirectoryModified);
 			}
 		}
 	}
 	
+	private void notify(boolean isFile, Path path,
+			Consumer<Path> onFile, Consumer<Path> onDirectory) {
+		if (isFile) {
+			onFile.accept(path);
+		} else {
+			onDirectory.accept(path);
+		}
+	}
 	
 	public interface Whistle {
 		
