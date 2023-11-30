25b99d0dacfffe8582304df7e9e1343009edb4aa
snagon <snagon@yeah.net>
2023-11-30 14:52:41 +0800

    新增: cglib代理新helper类

---
+++ src/main/java/com/kxindot/goblin/proxy/ProxyCallbackHelper.java
@@ -0,0 +1,38 @@
+package com.kxindot.goblin.proxy;
+
+import static com.kxindot.goblin.Objects.isNotEmpty;
+import static com.kxindot.goblin.Reflections.EMP_CLS_ARR;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import net.sf.cglib.proxy.NoOp;
+
+/**
+ * @author ZhaoQingJiang
+ */
+public class ProxyCallbackHelper extends CallbackHelper {
+	
+	public ProxyCallbackHelper(Class<?> superclass, Callback... callbacks) {
+		this(superclass, EMP_CLS_ARR, callbacks);
+	}
+
+	public ProxyCallbackHelper(Class<?> superclass, Class<?>[] interfaces, Callback... callbacks) {
+		super(superclass, interfaces, callbacks);
+	}
+
+	@Override
+	protected Object getCallback(Method method) {
+		Class<?> type = getSuperClass();
+		List<Callback> callbacks = list;
+		if (isNotEmpty(callbacks)) {
+			for (Callback callback : callbacks) {
+				if (callback.accept(type, method)) {
+					return callback;
+				}
+			}
+		}
+		return NoOp.INSTANCE;
+	}
+
+}
