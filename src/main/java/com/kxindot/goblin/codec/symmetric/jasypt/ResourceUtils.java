0b3a410479f1e5e2ad671fd8ded60e3001c49cc1
Swift Win <swiftwin@163.com>
2024-07-25 10:22:36 +0800

    Add Jasypt symmetric encryption tool

---
+++ src/main/java/com/kxindot/goblin/codec/symmetric/jasypt/ResourceUtils.java
@@ -0,0 +1,37 @@
+package com.kxindot.goblin.codec.symmetric.jasypt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+
+/**
+ * A utility class to access resources in src/main/resources
+ *
+ * @author 2024-07-15
+ */
+public class ResourceUtils {
+
+    public static String loadTxtFileFromResource(String fileName) {
+        StringBuilder contentBuilder = new StringBuilder();
+        try {
+            InputStream inputStream = ResourceUtils.class.getClassLoader().getResourceAsStream(fileName);
+            if (inputStream != null) {
+                // 使用BufferedReader逐行读取文件内容并附加到StringBuilder中
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        contentBuilder.append(line).append("\n");
+                    }
+                }
+                return contentBuilder.toString().trim();
+            } else {
+                throw new IllegalStateException("inputStream is null as expected");
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+}
